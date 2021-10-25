da8a7ec4199c035dcf8b10ab93eb7c6e12f38d7f
Sam <Samuelrichesprivate@gmail.com>
2021-10-26 00:19:59 +0100

    accidentally made a new git page

--- gui.py
+++ gui.py
@@ -5,6 +5,7 @@ import tkinter as tk
 import autosort
 
 
+errorMessage = "this is where errors will be seen"
 
 # gives the window some values
 root = tk.Tk()
@@ -14,14 +15,15 @@ root.resizable(0,0)
 root.title("Download Folder Sorter")
 
 
+out = tk.StringVar()
+out.set(errorMessage)
 
 # a label is text, where you can give it different arguments
 lab = tk.Label(root, text="Download folder Sorter", font="courier 25 bold", bg="#5e5e5e")
 
-#lambda represents an un-named function.
-but = tk.Button(root, text="Sort!", font="courier 15 bold", bg="gray", command=lambda: autosort.main())
+#TODO need to find a way to loop the autosort or atleast get it running well
+but = tk.Button(root, text="Sort!", font="courier 15 bold", bg="gray", command=lambda:[autosort.main(), out.set(autosort.err)])
 
-out = tk.StringVar()
 
 # you can also write the widget like this way
 disp = tk.Message(
@@ -32,11 +34,11 @@ disp = tk.Message(
     font=('courier', 15)
 )
 
-
+#TODO come back to this to center via a better method, probably whitespace with empty columns
 #grid section
-lab.grid(row=0,pady=50, padx=15)
-but.grid(row=1,pady=25, padx=265)
-disp.grid(row=2,pady=20, padx=100)
+lab.grid(row=0,padx=90,pady=45)
+but.grid(row=1,pady=15)
+disp.grid(row=2,pady=25)
 
 #shows window
 root.mainloop()
